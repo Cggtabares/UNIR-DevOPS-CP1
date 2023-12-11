f189fd434e4aa4cd9234d61c9820e1f0bac4d9f4
Cggtabares <carlosggtabares@gmail.com>
2023-12-11 13:16:16 -0400

    Descarga y subida de repositorio a Github

---
+++ test/rest/api_test.py
@@ -0,0 +1,39 @@
+import http.client
+import os
+import unittest
+from urllib.request import urlopen
+
+import pytest
+
+BASE_URL = "http://localhost:5000"
+BASE_URL_MOCK = "http://localhost:9090"
+DEFAULT_TIMEOUT = 2  # in secs
+
+@pytest.mark.api
+class TestApi(unittest.TestCase):
+    def setUp(self):
+        self.assertIsNotNone(BASE_URL, "URL no configurada")
+        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/add/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "3", "ERROR ADD"
+        )
+
+    def test_api_sqrt(self):
+        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "8", "ERROR SQRT"
+        )
+
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
---
+++ test/unit/calc_test.py
@@ -0,0 +1,66 @@
+import pytest
+import unittest
+
+from app.calc import Calculator
+
+
+@pytest.mark.unit
+class TestCalculate(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.add(2, 2))
+        self.assertEqual(0, self.calc.add(2, -2))
+        self.assertEqual(0, self.calc.add(-2, 2))
+        self.assertEqual(1, self.calc.add(1, 0))
+
+    def test_add_method_returns_correct_result1(self):
+        self.assertEqual(6, self.calc.add(3, 3))
+        self.assertEqual(0, self.calc.add(2, -2))
+        self.assertEqual(0, self.calc.add(-2, 2))
+        self.assertEqual(1, self.calc.add(1, 0))      
+        
+    def test_divide_method_returns_correct_result(self):
+        self.assertEqual(1, self.calc.divide(2, 2))
+        self.assertEqual(1.5, self.calc.divide(3, 2))
+        self.assertRaises(TypeError, self.calc.divide, "2", 2)
+
+    def test_add_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.add, "2", 2)
+        self.assertRaises(TypeError, self.calc.add, 2, "2")
+        self.assertRaises(TypeError, self.calc.add, "2", "2")
+        self.assertRaises(TypeError, self.calc.add, None, 2)
+        self.assertRaises(TypeError, self.calc.add, 2, None)
+        self.assertRaises(TypeError, self.calc.add, object(), 2)
+        self.assertRaises(TypeError, self.calc.add, 2, object())
+
+    def test_divide_method_fails_with_nan_parameter(self):
+        self.assertRaises(TypeError, self.calc.divide, "2", 2)
+        self.assertRaises(TypeError, self.calc.divide, 2, "2")
+        self.assertRaises(TypeError, self.calc.divide, "2", "2")
+
+    def test_multiply_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.multiply(2, 2))
+        self.assertEqual(0, self.calc.multiply(1, 0))
+        self.assertEqual(0, self.calc.multiply(-1, 0))
+        self.assertEqual(-2, self.calc.multiply(-1, 2))
+        self.assertRaises(TypeError, self.calc.multiply, "0", 0)
+        
+    def test_power_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.power(2, 2))
+        self.assertEqual(1, self.calc.power(1, 0))
+        self.assertEqual(1, self.calc.power(-1, 0))
+        self.assertEqual(-27, self.calc.power(-3, 3))
+        self.assertRaises(TypeError, self.calc.power, "0", 0)
+        
+    def test_substract_method_returns_correct_result(self):
+        self.assertEqual(4, self.calc.substract(10, 6))
+        self.assertEqual(-2, self.calc.substract(256, 258))
+        self.assertEqual(-1, self.calc.substract(-1, 0))
+        self.assertEqual(0, self.calc.substract(0, 0))
+        self.assertEqual(0, self.calc.substract(0, 0))
+        self.assertRaises(TypeError, self.calc.substract, "0", 0)
+        
+if __name__ == "__main__":  # pragma: no cover
+    unittest.main()
