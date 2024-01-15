4f878dc20cfb9772260d593fb9b7423cbbc9f23c
Cggtabares <carlosggtabares@gmail.com>
2024-01-15 18:36:43 -0400

    Update calc_test.py to divide by 0

--- test/unit/calc_test.py
+++ test/unit/calc_test.py
@@ -61,6 +61,10 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertEqual(0, self.calc.substract(0, 0))
         self.assertRaises(TypeError, self.calc.substract, "0", 0)
+
+    def test_divide_method_fails_with_zero_parameter(self):
+        self.assertRaises(TypeError, self.calc.divide, 2, 0)
+        
         
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
